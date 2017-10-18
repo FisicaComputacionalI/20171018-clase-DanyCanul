9999d8b24d8c0c22c070702661fb470c18bae44b
DanyCanul <31377538+DanyCanul@users.noreply.github.com>
2017-10-18 13:29:08 -0500

    Add files via upload
    Trabajo 2

---
+++ suma.cc
@@ -0,0 +1,18 @@
+//Programa que suma los numeros pares entre el 1 y 20
+//Autor: Daniela Canul Garcia
+//Creado el 18 de Octubre de 2017
+#include <iostream>
+using namespace std;
+int main ()
+{
+ int sum=0;
+ int count=1;
+ while(count<=20){
+     if ((count%2)==0){
+       sum=sum+count;
+      }
+     count++;
+ }
+ cout<<sum<<endl;
+return 0;
+}
